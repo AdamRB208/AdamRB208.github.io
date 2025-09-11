12d87108c2301010c8b1706759afe27686cab354
AdamRB208 <adamrborup@gmail.com>
2025-09-11 16:49:32 -0600

    Removed data from router and appstate.

--- src/AppState.js
+++ src/AppState.js
@@ -4,7 +4,5 @@ import { reactive } from 'vue'
 export const AppState = reactive({
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
-  /** @type {import('./models/Account.js').Account} user info from the database*/
-  account: null
 })
 
