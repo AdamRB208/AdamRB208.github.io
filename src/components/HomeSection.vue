b2cf715a2bd43f3624542ba3546214e3d6242531
AdamRB208 <adamrborup@gmail.com>
2025-09-19 14:36:36 -0600

    deploy

--- src/components/HomeSection.vue
+++ src/components/HomeSection.vue
@@ -6,8 +6,8 @@
 <template>
   <div class="col-12 d-flex justify-content-center mt-5 align-items-center" id="home">
     <div class="d-block justify-content-center mt-3 ms-5">
-      <p class="text-center fs-2 home-text">Hello, I'm Adam Borup.</p>
-      <p class="fs-5 home-text">A Fullstack Software Developer.</p>
+      <p class="text-center fs-2 home-text">Hello, I'm Adam Borup</p>
+      <p class="fs-5 home-text">A Fullstack Software Developer</p>
     </div>
   </div>
 </template>
@@ -17,4 +17,5 @@
 .home-text {
   color: #87beb8;
 }
+
 </style>
