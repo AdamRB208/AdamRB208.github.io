4f5eaf8d7355304aee3e524e51c088edbc7992b9
AdamRB208 <adamrborup@gmail.com>
2025-09-30 14:45:37 -0600

    @ click working on project card for source code button.

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -5,9 +5,17 @@ import { onMounted } from 'vue';
 
 const projects = [
   { name: 'Tower', icons: '', imgPath: "", path_1: '', path_2: "https://github.com/AdamRB208/tower_2", description: "Tower is a fullstack application with a Vue front end and a Node back end. The application is designed to be a social media style of application where users can get posts from other users, post, delete and update their own content." },
-  { name: 'Keepr', icons: '', imgPath: "", path_1: "", path_2: "", description: "Keepr is a fullstack application with a Vue front end and a C3 and SQL back end. The application is designed to be a social media style of application where users can get posts from other users, post, delete and update their own content." },
-  { name: 'Post-It', icons: '', imgPath: "", path_1: "", path_2: "", description: "Post It is a fullstack application with a Vue front end and a C# and SQL back end. The application is designed to be a social media style of application where users can get posts from other users, post, delete and update their own content." },
+  { name: 'Keepr', icons: '', imgPath: "", path_1: "", path_2: "https://github.com/AdamRB208/keepr_dotnet_fullstack", description: "Keepr is a fullstack application with a Vue front end and a C3 and SQL back end. The application is designed to be a social media style of application where users can get posts from other users, post, delete and update their own content." },
+  { name: 'Post-It', icons: '', imgPath: "", path_1: "", path_2: "https://github.com/AdamRB208/post_it_dotnet_fullstack", description: "Post It is a fullstack application with a Vue front end and a C# and SQL back end. The application is designed to be a social media style of application where users can get posts from other users, post, delete and update their own content." },
 ]
+
+function liveAppClick(path_1) {
+  window.open(path_1, '_blank');
+}
+
+function sourceCodeClick(path_2) {
+  window.open(path_2, '_blank');
+}
 
 
 </script>
@@ -18,11 +26,11 @@ const projects = [
     <img
       src="https://plus.unsplash.com/premium_photo-1720032305135-a87c5e0888b6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fHdlYiUyMGFwcGxpY2F0aW9uc3xlbnwwfHwwfHx8MA%3D%3D"
       :alt="project.name">
-    <div class="fs-5 mt-2 mb-2">{{ project.description }}</div>
+    <div class="fs-5 mt-2 mb-2">{{ project.name }}</div>
     <p>{{ project.description }}</p>
     <div class="d-flex justify-content-evenly mt-4">
-      <button class="btn btn-glow" type="button"><a :href="project.path_1" target="_blank"></a>Live Application</button>
-      <button class="btn btn-glow">Source Code <a href=""></a></button>
+      <button @click="liveAppClick(project.path_1)" class="btn btn-glow" type="button">Live Application</button>
+      <button @click="sourceCodeClick(project.path_2)" class="btn btn-glow">Source Code</button>
     </div>
   </div>
 </template>
