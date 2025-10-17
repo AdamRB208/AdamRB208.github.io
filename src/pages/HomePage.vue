d19a2c53a0c9c501b7760a64b0195f60d381b036
AdamRB208 <adamrborup@gmail.com>
2025-10-17 13:21:46 -0600

    deploy

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -46,10 +46,10 @@ import SkillsSection from '@/components/SkillsSection.vue';
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
-      width: 100%;
+      min-width: 100%;
         height: 100dvh;
         overflow-x: hidden;
-        padding: none;
+        padding: 0;
         --bs-gutter-x: 0;
 }
 </style>
